b9b19e34e8e01642e8df4dc85d5de5152c119226
Jennie Dalgren <jennie.dalgren@gmail.com>
2024-09-02 22:31:07 +0200

    Update index.js
    fix typos

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 1) (This has a DEMO solution.)
 Create a variable that stores the name of your cafe.
 Print out a greeting such as "Welcome to Technigo Cafe! What would you like to order today?"
-Where echnigo Cafe is replaced by the name of your cafe that is stored in the variable.
+Where Technigo Cafe is replaced by the name of your cafe that is stored in the variable.
 */
 const cafeName = "Technigo Cafe";
 console.log(`Welcome to ${cafeName}! What would you like to order today?`);
@@ -27,7 +27,7 @@ Where true is replaced by your varible
 Create a variable called cafeGuests, that shows us how many cafeGuests we have.
 Assign it a value.
 Print it out.
-assign it a new value.
+Assign it a new value.
 Print it out. => This should give you the new value.
 */
 
