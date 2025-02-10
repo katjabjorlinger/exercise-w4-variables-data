af8326ada1696bd282ab7266bdce50d0b7be36e3
katjabjorlinger <130973201+katjabjorlinger@users.noreply.github.com>
2025-02-10 21:29:47 +0100

    excersie completed

--- src/index.js
+++ src/index.js
@@ -14,6 +14,10 @@ Create a variable that stores how many coffees the customer wants.
 Print out the total price such as "There you go, that'll be 10 euros"
 Where 10 is replaced by the calculation of the total price.
 */
+const coffeePrice = 25; 
+let customerOrder = 2;
+
+console.log(`Varsågod, det blir ${coffeePrice * customerOrder} kronor`);
 
 /*
 3)
@@ -21,6 +25,9 @@ Create a variable that stores a boolean.
 Print out "You said this coffee is the best, that was actually true"
 Where true is replaced by your varible
 */
+
+let coffeeBest = true; 
+console.log(`You said this coffee is the best, that was actually ${coffeeBest}`);
 
 /*
 4)
@@ -31,6 +38,12 @@ Assign it a new value.
 Print it out. => This should give you the new value.
 */
 
+let guestCount = 30; 
+console.log(`Total guest are ${guestCount}`);
+
+guestCount = 49; 
+console.log(`Total guest are ${guestCount}`);
+
 /*
 5)
 Create a variable called maxGuests, that shows us how many guests are allowed in the cafe.
@@ -40,16 +53,26 @@ assign it a new value.
 Print it out. => This should give you an error because it shouldn't be able to be changed.
 */
 
+const maxGuests = 50; 
+console.log(maxGuests);
+
+/* maxGuests = 100;
+console.log(maxGuests); */
+
 /*
 6)
 Create a variable that stores a string.
 Print out that string in only UPPERCASE letters.
 */
 
+let myString = "Hello, welcome to the cafe!";
+console.log(myString.toUpperCase());
+
 /*
 7)
 Print out the same string in only lowercase letters.
 */
+console.log(myString.toLowerCase());
 
 /*
 8) **BONUS**
@@ -58,3 +81,7 @@ Then figure out a way to replace the word "summer" in the string with the word "
 Should give you => "Today we have a special winter deal!"
 (check for a specific string method...)
 */
+
+let specialDeal = "Today we have a special summer deal!";
+let newDeal = specialDeal.replace("summer", "winter");
+console.log(newDeal);
